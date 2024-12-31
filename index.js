1454b4c2dce848f607f9624f72cb8ac40a050bd8
ravinder singh <ravinders590@gmail.com>
2024-12-31 14:42:22 +0530

    404 error page and some component.

--- index.js
+++ index.js
@@ -11,7 +11,18 @@ app.use(express.static('./public'));
 app.get('/', (req, res) => {
   res.render('index');
 })
+app.get('*', (req, res) => {
+  throw Error("The page you were looking for could not be found!!")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+app.use(function errorHandler (err, req, res, next) {
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('404', { error: err })
+}
+);
